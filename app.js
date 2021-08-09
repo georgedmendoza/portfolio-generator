4ee213fa3dee695b792850b4825ab8289d525a90
georgedmendoza <georgedmendoza13@gmail.com>
2021-08-09 16:07:29 -0500

    create file and use require

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
+const[name,github] = profileDataArgs;
 
-    //is the same as this..
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
